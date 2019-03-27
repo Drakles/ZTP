ae9b7e107290669c2dc0427eaa812a624ff904d2
Kajetan <drakles1@gmail.com>
2019-03-27 15:48:01 +0100

    polisching

--- src/main/java/Task3/Main.java
+++ src/main/java/Task3/Main.java
@@ -8,10 +8,9 @@ import org.apache.commons.lang.SerializationUtils;
 public class Main {
 
   public static void main(String[] args){
-    List<University> universities = new ArrayList<>(Generator.generate(1, 50, 1000));
+    List<University> universities = new ArrayList<>(Generator.generate(1, 10, 1000));
 
-    University uni = universities.get(0);
-    int repeat = 100;
+    int repeat = 1;
     try {
 //      University clone = uni.clone();
 //
@@ -31,24 +30,29 @@ public class Main {
 //      System.out.println(serializedUni == uni);
 //      System.out.println(serializedUni.equals(uni));
 
-      long time = System.currentTimeMillis();
-      for (int i = 0; i < repeat; i++) {
-        testCloning(uni);
+      long time = System.nanoTime();
+      for (University uni : universities) {
+        for (int i = 0; i < repeat; i++) {
+          testCloning(uni);
+        }
       }
-      System.out.println("After cloning: " + (System.currentTimeMillis()-time));
+      System.out.println("After cloning: " + ((System.nanoTime()-time)/repeat));
 
-      time = System.currentTimeMillis();
-
-      for (int i = 0; i < repeat; i++) {
-        testCopyConstructor(uni);
+      time = System.nanoTime();
+      for (University uni : universities) {
+        for (int i = 0; i < repeat; i++) {
+          testCopyConstructor(uni);
+        }
       }
-      System.out.println("After copy constructor: " + (System.currentTimeMillis()-time));
+      System.out.println("After copy constructor: " + ((System.nanoTime()-time)/repeat));
 
-      time = System.currentTimeMillis();
-      for (int i = 0; i < repeat; i++) {
-        testSerialization(uni);
+      time = System.nanoTime();
+      for (University uni : universities) {
+        for (int i = 0; i < repeat; i++) {
+          testSerialization(uni);
+        }
       }
-      System.out.println("After serialization: " + (System.currentTimeMillis()-time));
+      System.out.println("After serialization: " + ((System.nanoTime()-time)/repeat));
 
     } catch (CloneNotSupportedException e) {
       e.printStackTrace();
